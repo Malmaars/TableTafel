7f2d2556f1e9cfaef2b94063f7ce3842b110728f
Smos Bois <simondeb1505@gmail.com>
2023-11-14 14:44:28 +0100

    Still working on the calibration

--- Assets/Scripts/BlackBoard.cs
+++ Assets/Scripts/BlackBoard.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public static class BlackBoard
 {
     public static Vector3 offset;
+
+    public static Vector2 center;
+    public static Vector2 PosX;
+    public static Vector2 NegX;
+    public static Vector2 Posy;
+    public static Vector2 NegY;
+
+
+
+    public static float Xratio;
+    public static float YRatio;
 }
--- Assets/Scripts/CalibrationManager.cs
+++ Assets/Scripts/CalibrationManager.cs
@@ -10,6 +10,9 @@ public class CalibrationManager : MonoBehaviour
     public int calibrationFiducial;
     public FiducialController fiducialTracker;
 
+    //0 = center, 1 = +X, 2 = -X, 3 = +Y, 4 = -Y
+    public Transform[] fiducialCalibrators;
+
     public Transform digitalTable;
     //if calibrating is 0, there is no calibration. Above 0 is calibrating, which will go through the steps counting up.
     int calibrating;
@@ -48,14 +51,85 @@ public class CalibrationManager : MonoBehaviour
                     }
                     break;
                 case 2:
-                    if (CalibrateZoom())
+                    if (liftFiducial || CalibrateZoom())
                     {
-                        calibrating = 0;
+                        //give a prompt to lift the tracker from the table
+                        if (LiftTracker())
+                        {
+                            liftFiducial = false;
+                            calibrating++;
+                        }
                     }
+                    break;
+                case 3:
+                    SetCalibrateFiducialPositions();
+                    calibrating++;
+                    break;
+                case 4:
+                    if (liftFiducial || CalibrateFiducialPositions(0))
+                    {
+                        //give a prompt to lift the tracker from the table
+                        if (LiftTracker())
+                        {
+                            liftFiducial = false;
+                            calibrating++;
+                        }
+                    }
+                    break;
+                case 5:
+                    if (liftFiducial || CalibrateFiducialPositions(1))
+                    {
+                        //give a prompt to lift the tracker from the table
+                        if (LiftTracker())
+                        {
+                            liftFiducial = false;
+                            calibrating++;
+                        }
+                    }
+                    break;
+                case 6:
+                    if (liftFiducial || CalibrateFiducialPositions(2))
+                    {
+                        //give a prompt to lift the tracker from the table
+                        if (LiftTracker())
+                        {
+                            liftFiducial = false;
+                            calibrating++;
+                        }
+                    }
+                    break;
+                case 7:
+                    if (liftFiducial || CalibrateFiducialPositions(3))
+                    {
+                        //give a prompt to lift the tracker from the table
+                        if (LiftTracker())
+                        {
+                            liftFiducial = false;
+                            calibrating++;
+                        }
+                    }
+                    break;
+                case 8:
+                    if (liftFiducial || CalibrateFiducialPositions(4))
+                    {
+                        //give a prompt to lift the tracker from the table
+                        if (LiftTracker())
+                        {
+                            liftFiducial = false;
+                            calibrating++;
+                        }
+                    }
+                    break;
+                case 9:
+                    CalibrateFIducialAspectRatio();
+                    calibrating = 0;
                     break;
             }
                 
         }
+
+        if (Input.GetKeyDown(KeyCode.C))
+            calibrating = 1;
     }
 
     public void StartCalibration()
@@ -72,7 +146,7 @@ public class CalibrationManager : MonoBehaviour
         Vector2 newCenter = new Vector2(0, 0);
         newCenter = new Vector2(-fiducialTracker.screenPosition.x * 10 + 5, -fiducialTracker.screenPosition.y * 10 + 5);
         BlackBoard.offset = new Vector3(newCenter.x, 0, newCenter.y);
-        mainCamera.transform.position = new Vector3(-newCenter.x, mainCamera.transform.position.y, -newCenter.y);
+        digitalTable.transform.position = new Vector3(-newCenter.x, mainCamera.transform.position.y, -newCenter.y);
 
         //if the fiducial is on the same place long enough, end the calibration.
 
@@ -106,7 +180,7 @@ public class CalibrationManager : MonoBehaviour
         //change the size of the table, not the size of the camera
         //mainCamera.orthographicSize += direction.x * speed;
 
-        digitalTable.localScale += new Vector3(direction.x * speed, 0, direction.y * speed);
+        digitalTable.localScale += new Vector3(direction.x * speed, 0, direction.x * speed);
 
         if (fiducialTracker.ScreenPosition == lastPosition && fiducialTracker.isVisible)
         {
@@ -129,9 +203,82 @@ public class CalibrationManager : MonoBehaviour
         return false;
     }
 
-    void CalibrateAspectRatio()
+    void SetCalibrateFiducialPositions()
+    {
+
+        //first set the calibrators at the right position
+        foreach (Transform calibrator in fiducialCalibrators)
+        {
+            calibrator.position = BlackBoard.offset;
+            calibrator.gameObject.SetActive(false);
+        }
+        fiducialCalibrators[1].position += new Vector3(digitalTable.localScale.x, 2, 0);
+        fiducialCalibrators[2].position += new Vector3(-digitalTable.localScale.x, 2, 0);
+        fiducialCalibrators[3].position += new Vector3(0, 2, digitalTable.localScale.z);
+        fiducialCalibrators[4].position += new Vector3(0, 2, -digitalTable.localScale.z);
+    }
+
+    bool CalibrateFiducialPositions(int whichCalibrator)
+    {
+        //put one square in the center, and four squares at designated points (like 0.5, or 0.8) use these to create an accurate measurement of the table
+
+        foreach (Transform calibrator in fiducialCalibrators)
+        {
+            calibrator.gameObject.SetActive(false);
+        }
+        //then one by one match the fiducial with the calibrators
+
+        switch (whichCalibrator)
+        {
+            case 0:
+                //this is the center
+                fiducialCalibrators[0].gameObject.SetActive(true);
+                BlackBoard.center = fiducialTracker.screenPosition;
+                break;
+            case 1:
+                fiducialCalibrators[1].gameObject.SetActive(true);
+                BlackBoard.PosX = fiducialTracker.screenPosition;
+                break;
+            case 2:
+                fiducialCalibrators[2].gameObject.SetActive(true);
+                BlackBoard.NegX = fiducialTracker.screenPosition;
+                break;
+            case 3:
+                fiducialCalibrators[3].gameObject.SetActive(true);
+                BlackBoard.Posy = fiducialTracker.screenPosition;
+                break;
+            case 4:
+                fiducialCalibrators[4].gameObject.SetActive(true);
+                BlackBoard.NegY = fiducialTracker.screenPosition;
+                break;
+
+        }
+
+        if (fiducialTracker.ScreenPosition == lastPosition && fiducialTracker.isVisible)
+        {
+            calibrationtimer += Time.deltaTime;
+            if (calibrationtimer > timeToEndCalibration)
+            {
+                //end this calibration
+                calibrationtimer = 0;
+                liftFiducial = true;
+                return true;
+            }
+        }
+        else
+        {
+            calibrationtimer = 0;
+        }
+
+        lastPosition = fiducialTracker.ScreenPosition;
+        return false;
+    }
+
+    void CalibrateFIducialAspectRatio()
     {
         //change the aspect ratio of the camera to combat slight change in perspective from the beamer
+        BlackBoard.Xratio = BlackBoard.PosX.x * 2;
+        BlackBoard.YRatio = BlackBoard.Posy.y * 2;
     }
 
 
