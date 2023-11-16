a6634a5ac0a416ecd9c01129ab749bd8ebc9b53b
Smos Bois <simondeb1505@gmail.com>
2023-11-16 20:50:01 +0100

    Added Hexagons

--- Assets/Scripts/GridGenerator.cs
+++ Assets/Scripts/GridGenerator.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class GridGenerator : MonoBehaviour
 {
-    public float cubeSize;
-    public GameObject tryPrefab;
+    public Vector2 gridSize;
+    public Vector2 distance;
+    public float rowOffset;
+    public GameObject gridPart;
     // Start is called before the first frame update
     void Start()
     {
        //generate the Cubes
-       for(float i = -6; i < 6; i+=0.2f)
+       for(float i = -gridSize.x / 2; i < gridSize.x / 2; i++)
         {
-            for(float k = -6; k <6; k+=0.2f)
+            for(float k = -gridSize.y / 2; k < gridSize.y / 2; k++)
             {
-                Instantiate(tryPrefab, new Vector3(i, 0, k), new Quaternion(0, 0, 0, 0));
+                if (k % 2 == 0)
+                    Instantiate(gridPart, new Vector3(i * distance.x, 0, k * distance.y), new Quaternion(0, 0, 0, 0));
+
+                else
+                    Instantiate(gridPart, new Vector3(i * distance.x + rowOffset, 0, k * distance.y), new Quaternion(0, 0, 0, 0));
             }
         }
     }
